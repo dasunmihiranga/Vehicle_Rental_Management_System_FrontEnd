484494adfbfe70345c794b3fb2c79cc2ad5240a3
Dasun Mihiranga <da12345sun@gmail.com>
2024-11-07 14:37:39 +0530

    get All vehicles from Api  and bind to customer dashboard

--- src/app/modules/customer/components/customer-dashboard/customer-dashboard.component.ts
+++ src/app/modules/customer/components/customer-dashboard/customer-dashboard.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { CustomerService } from '../../services/customer.service';
 
 @Component({
   selector: 'app-customer-dashboard',
   templateUrl: './customer-dashboard.component.html',
-  styleUrls: ['./customer-dashboard.component.scss']
+  styleUrls: ['./customer-dashboard.component.scss'],
 })
 export class CustomerDashboardComponent {
+  cars: any = [];
+  successMessage: string | null = null;
+  errorMessage: string | null = null;
+  constructor(private service: CustomerService) {}
 
+  ngOnInit() {
+    this.getAllCars();
+  }
+
+  getAllCars() {
+    this.service.getAllCars().subscribe((res: any[]) => {
+      console.log(res);
+      res.forEach((element: any) => {
+        element.processedImg =
+          'data:image/jpeg;base64,' + element.returnedImage;
+        this.cars.push(element);
+      });
+    });
+  }
+
+ 
 }
--- src/app/modules/customer/services/customer.service.ts
+++ src/app/modules/customer/services/customer.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { StorageService } from 'src/app/auth/services/storage/storage.service';
 
+const BASE_URL=['http://localhost:8080']
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getAllCars():Observable<any>{
+    return this.http.get(`${BASE_URL}/api/customer/cars`,{
+      headers : this.createAuthorizationHeader()
+    })
+  }
+
+  createAuthorizationHeader(): HttpHeaders {
+    let authHeadeaders: HttpHeaders = new HttpHeaders();
+    return authHeadeaders.set(
+      'Authorization',
+      'Bearer ' + StorageService.getToken()
+    );
+  }
+
+
 }
