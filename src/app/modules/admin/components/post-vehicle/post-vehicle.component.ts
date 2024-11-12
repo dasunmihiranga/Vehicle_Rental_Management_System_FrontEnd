9e970bc1d0f179f0c73f9e36c1336c2445146ce5
Dasun Mihiranga <da12345sun@gmail.com>
2024-11-13 01:23:25 +0530

    remove unused content in ts

--- src/app/modules/admin/components/post-vehicle/post-vehicle.component.ts
+++ src/app/modules/admin/components/post-vehicle/post-vehicle.component.ts
@@ -78,17 +78,16 @@ export class PostVehicleComponent implements OnInit {
       next: () => {
         this.successMessage = "Vehicle Post created successfully!";
         this.errorMessage = null;
-        this.autoDismissSuccess();  // Call the auto dismiss method
+        this.autoDismissSuccess();  
   
-        // Navigate to the login page after a delay to show the success message
         setTimeout(() => {
           this.router.navigateByUrl('/admin/dashboard');
-        }, 2000); // Delay of 5 seconds for the success message
+        }, 2000); 
       },
       error: (err) => {
         this.errorMessage = err.error.message || "Creation failed!";
         this.successMessage = null;
-        this.autoDismissError();  // Call the auto dismiss method
+        this.autoDismissError(); 
       }
     }); 
   }
@@ -114,14 +113,14 @@ export class PostVehicleComponent implements OnInit {
   autoDismissSuccess() {
     setTimeout(() => {
       this.successMessage = null;
-    }, 2000); // 5000 milliseconds = 5 seconds
+    }, 2000);
   }
 
-  // Auto-dismiss error message after 5 seconds
+
   autoDismissError() {
     setTimeout(() => {
       this.errorMessage = null;
-    }, 5000); // 5000 milliseconds = 5 seconds
+    }, 5000); 
   }
   
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,9 +11,9 @@ module.exports = {
         background1:'#1D1D1D',
         surface: '#353535', // Card elements color
         text: '#E0E4E8', // Text color
-        primary: '#1B68C4', // Primary button color
-        secondary: '#F6425E', // Secondary highlights color
-        muted: '#A5B3B7', // Light gray for less important text
+        primary: '#1B68C4', 
+        secondary: '#F6425E', 
+        muted: '#A5B3B7', 
       },
     },
   },
